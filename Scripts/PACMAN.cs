77c6710f4118679437c615b5594e276174462e05
ManavGhaiCode <manavghai12october@gmail.com>
2023-07-09 17:01:12 +0530

    adjust PACMAN speed increase

--- Scripts/PACMAN.cs
+++ Scripts/PACMAN.cs
@@ -122,7 +122,7 @@ public class PACMAN : MonoBehaviour {
             if (lifes > 0) {
                 agent.SetDestination(new Vector2 (0, 3.75f));
 
-                speed += .25f;
+                speed += .15f;
                 isResponing = true;
                 agent.speed = speed;
 
